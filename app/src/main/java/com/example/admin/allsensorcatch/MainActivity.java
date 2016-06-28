aadbafc63217c978010a7deacb9387b32172cf91
HorseDeer <sd.7r1i9c3e@gmail.com>
2016-06-29 01:44:17 +0900

    センサーの全タイプ取得とデータの表示

--- app/src/main/java/com/example/admin/allsensorcatch/MainActivity.java
+++ app/src/main/java/com/example/admin/allsensorcatch/MainActivity.java
@@ -9,41 +9,47 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener ,View.OnClickListener ,Serializable{
+public class MainActivity extends AppCompatActivity implements SensorEventListener, AdapterView.OnItemClickListener, Serializable{
+    //各種変数の定義
     private SensorManager sm;
-    private TextView sensorList;
-    private Button goButton;
+    private ArrayList<String> sensorType = new ArrayList<String>();
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sensorList = (TextView)findViewById(R.id.namelist);
+        //センサマネージャの設定とリストビューのID紐づけ
         sm = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
-        goButton = (Button)findViewById(R.id.sensorB);
-        goButton.setOnClickListener(this);
+        listView = (ListView)findViewById(R.id.namelist);
+        listView.setOnItemClickListener(this);
     }
 
     @Override
     protected void onResume() {
         // TODO Auto-generated method stub
         super.onResume();
+        //全ての種類のセンサーを取得する
         List<Sensor> sensors = sm.getSensorList(Sensor.TYPE_ALL);
-        String str = "実装されているセンサー一覧:\n\n";
-        for(Sensor s : sensors) {
-            str += s.getName() + "\n";
+       for(Sensor s : sensors) {
+           //String型のArrayListに追加していく
+           sensorType.add(s.getName());
         }
-        for(int i = 0; i < 100; i++) {
-            str+="✌('ω'✌ )三✌('ω')✌三( ✌'ω')✌ "+(i+1)+"\n";
-        }
-        sensorList.setText(str);
+        /*for(int i = 0; i < 100; i++) {
+            sensorType.add("✌('ω'✌ )三✌('ω')✌三( ✌'ω')✌ "+(i+1));
+        }*/
+        //ListViewにアダプターを設定する
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, sensorType);
+        listView.setAdapter(adapter);
     }
 
     @Override
@@ -52,6 +58,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if (sm != null) {
             sm.unregisterListener(this);
         }
+        sensorType.clear();
     }
 
     @Override
@@ -65,10 +72,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     @Override
-    public void onClick(View v) {
-        if (v == goButton) {
-            Intent intent = new Intent(MainActivity.this,SensorList.class);
-            startActivity(intent);
-        }
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        ListView clickList = (ListView) parent;
+        Intent intent = new Intent(MainActivity.this, SensorList.class);
+        intent.putExtra("SensorType", (String)clickList.getItemAtPosition(position));
+        //Toast.makeText(getApplicationContext(), intent.getStringExtra("SensorType"),Toast.LENGTH_LONG).show();
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/admin/allsensorcatch/SensorList.java
+++ app/src/main/java/com/example/admin/allsensorcatch/SensorList.java
@@ -1,6 +1,7 @@
 package com.example.admin.allsensorcatch;
 
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -8,6 +9,7 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -16,39 +18,27 @@ import java.util.List;
 
 public class SensorList extends AppCompatActivity implements SensorEventListener, Serializable {
     private SensorManager sm2;
-    private TextView sensorLists;
-    private ArrayList<String> datas = new ArrayList<String>();
+    private TextView data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sensorlayout);
-        sensorLists = (TextView)findViewById(R.id.sensorlist);
+        data = (TextView)findViewById(R.id.sensorData);
         sm2 = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
     }
 
-    //Gyroscope
-    //Accelerometer
-    //Magnetic
-    //Orientation
-    //Rotation
-    //Liner Acceleration
-    //Gravity
-    //Proximity sensor
-    //Light Sensor
-    //Rotation Vector Sensor
-    //Liner Acceleration Sensor
-    //Orientation Sensor
-    //Corrected Gyroscope Sensor
     @Override
     protected void onResume() {
         // TODO Auto-generated method stub
         super.onResume();
-
+        Intent intent = getIntent();
+        String type = intent.getStringExtra("SensorType");
         List<Sensor> sensors = sm2.getSensorList(Sensor.TYPE_ALL);
-
         for(Sensor s : sensors) {
-            sm2.registerListener(this,s,SensorManager.SENSOR_DELAY_NORMAL);
+            if (s.getName().equals(type)) {
+                sm2.registerListener(this,s,SensorManager.SENSOR_DELAY_UI);
+            }
         }
     }
 
@@ -61,39 +51,12 @@ public class SensorList extends AppCompatActivity implements SensorEventListener
     }
     @Override
     public void onSensorChanged(SensorEvent event) {
-        /*boolean isSet = false;
-        for (int i = 0; i < datas.size(); i = i+4) {
-            if (datas.get(i).equals(event.sensor.getName())) {
-                datas.set(i+1,event.values[0]+"");
-                datas.set(i+2,event.values[1]+"");
-                datas.set(i+3,event.values[2]+"");
-                isSet = true;
-            }
-            if (isSet)break;
-        }
-        if (!isSet) {
-            datas.add(event.sensor.getName());
-            datas.add(event.values[0]+"");
-            datas.add(event.values[1]+"");
-            datas.add(event.values[2]+"");
-        }*/
-        int search = datas.indexOf(event.sensor.getName());
-        if (search != -1) {
-            datas.set(search+1,event.values[0]+"");
-            datas.set(search+2,event.values[1]+"");
-            datas.set(search+3,event.values[2]+"");
-        }
-        else {
-            datas.add(event.sensor.getName());
-            datas.add(event.values[0]+"");
-            datas.add(event.values[1]+"");
-            datas.add(event.values[2]+"");
-        }
-        String str = "";
-        for (int i = 0; i < datas.size(); i++) {
-            str +=  datas.get(i)+"\n";
-        }
-        sensorLists.setText(str);
+        String result = "";
+        result += event.sensor.getName() + "\n\n";
+        result += event.values[0] + "\n";
+        result += event.values[1] + "\n";
+        result += event.values[2] + "\n";
+        data.setText(result);
     }
 
     @Override
